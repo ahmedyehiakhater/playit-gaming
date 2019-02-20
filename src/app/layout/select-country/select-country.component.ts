aed16064c1073c8ddc0dc4e2b2c68ab5465de3cd
Ahmed Yehia <a.khater@grandmstg.com>
2019-02-20 12:46:36 +0200

    Made default styling scss and and changed css files to scss

--- src/app/layout/select-country/select-country.component.ts
+++ src/app/layout/select-country/select-country.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-select-country',
   templateUrl: './select-country.component.html',
-  styleUrls: ['./select-country.component.css']
+  styleUrls: ['./select-country.component.scss']
 })
 export class SelectCountryComponent implements OnInit {
 
