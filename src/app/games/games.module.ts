938be41157952ec86815dad4db368c07c883ba67
Ahmed Yehia <a.khater@grandmstg.com>
2019-03-13 12:55:47 +0200

    Generated new component game-details

--- src/app/games/games.module.ts
+++ src/app/games/games.module.ts
@@ -1,14 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { GamesRoutingModule } from './games-routing.module';
 import { ListGamesComponent } from './list-games/list-games.component';
+import { GameDetailsComponent } from './game-details/game-details.component';
 
 @NgModule({
   imports: [
     CommonModule,
     GamesRoutingModule
   ],
-  declarations: [ListGamesComponent]
+  declarations: [ListGamesComponent, GameDetailsComponent]
 })
 export class GamesModule { }
